0b258d3a391363466d2646e92e25680fb2e653dc
alexzxz <wu.allen154@gmail.com>
2016-03-23 16:43:57 -0400

    convert to directive

--- sdd/website/public/js/app.js
+++ sdd/website/public/js/app.js
@@ -1,4 +1,4 @@
 // public/js/app.js
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService']);
+angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'app-directives']);
 
-$(function(){   $("#header-wrapper").load("/views/header.html");  });
+//$(function(){   $("#header-wrapper").load("/views/header.html");  });
--- sdd/website/public/libs/angular/directives.js
+++ sdd/website/public/libs/angular/directives.js
@@ -1,16 +1,24 @@
 // Directives store commonly used templates
 
-(function(){
-  var app = angular.module('directives', []);
 
-    app.directive("login", function() {
+(function(){
+  var app = angular.module('app-directives', []);
+
+ /* app.directive("login", function() {
     return {
       restrict: 'E',
       templateUrl: "login.html"
     };
   });
+*/
+  app.directive("headerWrapper", function() {
+  	return {
+  		restrict: 'E',
+  		templateUrl: "views/header.html"
+  	};
+  });
 
 
 
 
-};
+})();
