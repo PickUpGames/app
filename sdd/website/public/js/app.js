a1e36f0263efac35e1da9949946f49b74079837e
alexzxz <wu.allen154@gmail.com>
2016-03-27 22:12:03 -0400

    added login modal

--- sdd/website/public/js/app.js
+++ sdd/website/public/js/app.js
@@ -1,4 +1,5 @@
 // public/js/app.js
 angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'app-directives']);
 
-//$(function(){   $("#header-wrapper").load("/views/header.html");  });
+$(function(){   $("#modal-wrapper").load("/views/modal.html");  });
+
