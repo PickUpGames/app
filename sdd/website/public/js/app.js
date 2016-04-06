0356fe537537163a8b1ccaf2fef5f260897176a0
alexzxz <wu.allen154@gmail.com>
2016-04-06 17:44:17 -0400

    added signup

--- sdd/website/public/js/app.js
+++ sdd/website/public/js/app.js
@@ -2,5 +2,6 @@
 angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'EventCtrl', 'NerdService', 'app-directives']);
 
 $(function(){   $("#login-modal-wrapper").load("/views/modal/login.html");  });
+$(function(){   $("#signup-modal-wrapper").load("/views/modal/signup.html");  });
 $(function(){   $("#view-modal-wrapper").load("/views/modal/view-event.html");  });
 
